53593e12c66cd189248d3813139c2fd6af472e55
Chris Hunt <chris@tappetyclick.com>
2025-04-03 22:24:25 +0100

    Don't allow null for now

--- Attributes/DependentCachesAttribute.cs
+++ Attributes/DependentCachesAttribute.cs
@@ -1,14 +1,13 @@
-﻿namespace CleverCache.Attributes
+﻿namespace CleverCache.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class DependantCachesAttribute(
+	Type[] types,
+	DependentCacheNavigationScanMode navigationScanMode = DependentCacheNavigationScanMode.None,
+	bool reverse = false
+) : Attribute
 {
-    [AttributeUsage(AttributeTargets.Class)]
-    public class DependantCachesAttribute(
-        Type[]? types/* = null*/,
-        DependentCacheNavigationScanMode navigationScanMode = DependentCacheNavigationScanMode.None,
-        bool reverse = false
-    ) : Attribute
-    {
-        public Type[] DependantTypes { get; } = types ?? [];
-        public DependentCacheNavigationScanMode NavigationScanMode { get; set; } = navigationScanMode;
-        public bool Reverse { get; set; } = reverse;
-    }
+	public Type[] DependantTypes { get; } = types ?? [];
+	public DependentCacheNavigationScanMode NavigationScanMode { get; set; } = navigationScanMode;
+	public bool Reverse { get; set; } = reverse;
 }
